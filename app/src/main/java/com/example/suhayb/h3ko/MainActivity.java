0f09f3169a7f914a9538b61cc330cf57d463589c
suhayb <arsl94@hotmail.com>
2018-06-17 20:17:17 +0200

    login layout

--- app/src/main/java/com/example/suhayb/h3ko/MainActivity.java
+++ app/src/main/java/com/example/suhayb/h3ko/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.suhayb.h3ko;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, email.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void findViews() {
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.loginBtn);
     }
 }
