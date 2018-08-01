723caeffb6ec971ba8a9a0ad96e8c93f9509974c
SUTTROOGUN Yogin Kumar <ysuttroogun>
2018-08-01 21:48:47 +0400

    Adding medicinal data layout + saving the information + displaying the information

--- app/src/main/java/com/example/suhayb/h3ko/fragments/MedicinalDataFragment.java
+++ app/src/main/java/com/example/suhayb/h3ko/fragments/MedicinalDataFragment.java
@@ -1,14 +1,26 @@
 package com.example.suhayb.h3ko.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.suhayb.h3ko.R;
+import com.example.suhayb.h3ko.Utils.Constants;
 
 public class MedicinalDataFragment extends Fragment {
+
+    TextView conditions;
+    TextView treatment;
+    TextView medication;
+    TextView symptoms;
+    TextView heartBeat;
+
+    SharedPreferences mPreferences;
 
     public MedicinalDataFragment() {
         // Required empty public constructor
@@ -29,5 +41,18 @@ public class MedicinalDataFragment extends Fragment {
     }
 
     private void initViews(View view) {
+        conditions = view.findViewById(R.id.conditions);
+        treatment = view.findViewById(R.id.current_treatments);;
+        medication = view.findViewById(R.id.current_medication);;
+        symptoms = view.findViewById(R.id.symp);;
+        heartBeat = view.findViewById(R.id.heartBeat);
+
+        mPreferences = getContext().getApplicationContext().getSharedPreferences(Constants.PREF_FILE, Context.MODE_PRIVATE);
+
+        conditions.setText(mPreferences.getString(Constants.vorerkrankungen, ""));
+        treatment.setText(mPreferences.getString(Constants.aktuelle_behandlungen, ""));
+        medication.setText(mPreferences.getString(Constants.aktuelle_mediaktion, ""));
+        symptoms.setText(mPreferences.getString(Constants.aktuelle_vergangene_symptomen, ""));
+        heartBeat.setText(mPreferences.getString(Constants.herztoene, ""));
     }
 }
